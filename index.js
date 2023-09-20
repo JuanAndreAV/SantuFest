56b7dff5f6483dba2e7ec87b58a1b284a7a7b8ef
Juan Andres Alvarez <juanandres1029@gmail.com>
2023-09-20 15:47:50 -0500

    Descriptions

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ window.addEventListener('DOMContentLoaded', function (){
     spinner.classList.remove('d-none');
 })
 
-const restaurantes=["restaurante 1","restaurante 2","restaurante 3","restaurante 4"]
+const restaurantes=["ANTICUARIO","DONDE DONDO","EL SECRETO","FRUTOS DEL PARAISO","GREEN BURGER","HOJARASCA","LYLY COOK", "MACUNA","Q2","THADDEUS"]
 var caracteresEspeciales = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\|]/;
 
 
